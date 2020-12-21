2404406598890a3646821620f7fd40917c5885c8
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-12-21 19:17:25 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+// eslint-disable-next-line
+import regeneratorRuntime from "regenerator-runtime";
+import Menu from "./component/Menu.js";
+import HomePage from "./component/HomePage.js";
+import DriftPage from "./component/DriftPage.js";
+import TimeAttackPage from "./component/TimeAttackPage.js";
+import ForzaPage from "./component/ForzaPage.js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/drift" component={DriftPage} />
+          <Route path="/timeattack" component={TimeAttackPage} />
+          <Route path="/forza" component={ForzaPage} />
+        </div>
+      </div>
+    </Router>
   );
 }
-
-export default App;
